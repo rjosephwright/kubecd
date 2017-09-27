39c377579f7ed98ef0d683bc0d65f5cae8df5b9a
Joseph Wright <rjosephwright@gmail.com>
2017-09-27 00:59:14 -0400

    Add bare bones ability to run a Concourse task using `fly`
    The command line client for Concourse is called `fly`. It has the
    ability to execute a single task using the `fly execute` command.
    These additions to the HTTP routes makes it possible to kick off
    a Kubernetes job by running that command with a task input file.
    There is not yet any proper handling of log messages, and the
    response is not what the `fly` executable expects, but it
    nonetheless does start the job on Kubernetes.

--- lib/kubecd/web/router.ex
+++ lib/kubecd/web/router.ex
@@ -14,6 +14,22 @@ defmodule Kubecd.Web.Router do
     send_resp(conn, 200, encode!(%{message: "api"}))
   end
 
+  get "/api/v1/info" do
+    send_resp(conn, 200, encode!(%{version: "3.3.2"}))
+  end
+
+  post "/api/v1/builds" do
+    maps = conn.body_params["do"]
+    [task | _] = Enum.filter maps, fn map -> "task" in Map.keys map; end
+    job = Kubecd.Task.concourse_task_to_k8s_job(task["task"]["config"])
+    Kubecd.Task.run_job(job, "ci")
+    send_resp(conn, 400, encode!(%{message: "api"}))
+  end
+
+  post "/api/v1/pipes" do
+    send_resp(conn, 200, encode!(%{message: "api"}))
+  end
+
   match _ do
     send_resp(conn, 404, encode!(%{error: "Not found"}))
   end
